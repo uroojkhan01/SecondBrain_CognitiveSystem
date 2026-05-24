9253939b0dd2620e4d6380f961cf031e2a60de0c
uroojkhan01 <urjkhan01@gmail.com>
2026-05-24 15:03:50 +0200

    adding requirement httpx

--- assistant_backend_1/config.py
+++ assistant_backend_1/config.py
@@ -7,5 +7,5 @@ if os.path.exists(".env"):
     load_dotenv(".env")
 
 # now we have them as a handy python strings!
-TELEGRAM_BOT_TOKEN = os.getenv('BOT_TOKEN')
-TELEGRAM_BOT_USERNAME = os.getenv('BOT_USERNAME')
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+TELEGRAM_BOT_USERNAME = os.getenv('TELEGRAM_BOT_USERNAME')
