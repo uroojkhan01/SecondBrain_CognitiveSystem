e2323a3e0ab6b8d81f785da6486b61388ae1c92c
uroojkhan01 <urjkhan01@gmail.com>
2026-05-27 13:55:26 +0200

    Add telegram voice to text conversion

--- assistant_backend_1/api/handlers/telegram_handler.py
+++ assistant_backend_1/api/handlers/telegram_handler.py
@@ -1,5 +1,10 @@
 from fastapi import Request
-from assistant_backend_1.features_services.telegram import send_message
+from assistant_backend_1.features_services.telegram import (
+    send_message,
+    process_voice_message
+)
+from assistant_backend_1.features_services.voice_to_text import transcribe_audio_file
+import asyncio
 
 
 async def telegram_webhook(request: Request):
@@ -14,9 +19,32 @@ async def telegram_webhook(request: Request):
         return {"status": "ignored"}
 
     chat_id = message["chat"]["id"]
-    text = message.get("text", "")
 
-    reply = f"You said: {text}"
+    if "voice" in message:
+
+        voice = message["voice"]
+
+        voice_file_id = voice["file_id"]
+        voice_duration = voice["duration"]
+
+        print(f"Voice message received! Duration: {voice_duration} seconds.")
+
+        # process voice in service
+        audio_path = await process_voice_message(file_id=voice_file_id)
+        transcript = await asyncio.to_thread(
+            transcribe_audio_file,
+            audio_path
+        )
+
+        reply = f"You said (voice): {transcript}"
+
+    else:
+
+        text = message.get("text", "")
+
+        print(f"Text message received: {text}")
+
+        reply = f"You said: {text}"
 
     await send_message(chat_id, reply)
 
