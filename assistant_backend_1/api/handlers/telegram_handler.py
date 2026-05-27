cbdd912084c409d9ca5ad083975a0212dc9e2e28
Urooj Irfat <43551177+uroojkhan01@users.noreply.github.com>
2026-05-27 23:34:05 +0200

    Update telegram_handler.py
    testing for voice to text , from feature voice to text branch

--- assistant_backend_1/api/handlers/telegram_handler.py
+++ assistant_backend_1/api/handlers/telegram_handler.py
@@ -1,7 +1,4 @@
 from fastapi import Request
-from assistant_backend_1.features_services.telegram import send_message
-from assistant_backend_1.helpers import save_user,get_oauth_url, load_users,is_notion_connected 
-
 from assistant_backend_1.features_services.telegram import (
     send_message,
     process_voice_message
@@ -21,6 +18,8 @@ async def telegram_webhook(request: Request):
     if not message:
         return {"status": "ignored"}
 
+    chat_id = message["chat"]["id"]
+
     if "voice" in message:
 
         voice = message["voice"]
@@ -36,7 +35,7 @@ async def telegram_webhook(request: Request):
             transcribe_audio_file,
             audio_path
         )
-
+        print(transcript)
         reply = f"You said (voice): {transcript}"
 
     else:
@@ -47,26 +46,6 @@ async def telegram_webhook(request: Request):
 
         reply = f"You said: {text}"
 
-    chat_id = str(message["chat"]["id"])
-    first_name = message["chat"].get("first_name")
-    username = message["chat"].get("username")
-    text = message.get("text", "")
-
-    # Save user to JSON
-    save_user(chat_id, first_name, username)
-    print(f"Current users: {load_users()}")
-
-    
-    if not is_notion_connected(chat_id):
-        oauth_url = get_oauth_url(chat_id)
-        await send_message(
-            chat_id,
-            f"👋 Welcome! Please connect your Notion account to get started:\n\n"
-            f"🔗 {oauth_url}"
-        )
-        return {"status": "ok"}
-
-    # Notion is connected — handle message normally
     await send_message(chat_id, reply)
+
     return {"status": "ok"}
-   
